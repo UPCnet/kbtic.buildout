60e42383cd74af99b5492be1eeb367f3224fdb86
Roberto Diaz <roberto.diaz@upcnet.es>
2013-01-10 13:24:23 +0100

    config buildout and links template tiny

---
+++ produccio/links.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ln -s /var/plone/kbtic.buildout/produccio/o2k7 /var/plone/kbtic.buildout/produccio/eggs/Products.TinyMCE-1.2.10-py2.6.egg/Products/TinyMCE/skins/tinymce/themes/advanced/skins
